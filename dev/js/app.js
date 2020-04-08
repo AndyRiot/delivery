e311177ecb2bfa624a938ad4b3794cff7be8315e
AndyRiot <endy.q91@gmail.com>
2020-04-08 18:35:37 +0300

    slider

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,3 +2,25 @@
 
 import '../scss/app.scss';
 
+(() => {
+    setInterval(() => {
+        let sliders = document.querySelectorAll('.slider');
+
+        sliders.forEach(() => {
+            let currentSlide = document.querySelector('.active-slide'),
+                next = currentSlide.nextElementSibling;
+
+            console.log(next);
+
+            if (!next) {
+                next = currentSlide.parentElement.querySelector('.promo'); // first
+            }
+
+            next.classList.add('active-slide');/**/
+            currentSlide.classList.remove('active-slide');
+
+            setTimeout(() => {
+            }, 500);
+        });
+    }, 5000);
+})();
